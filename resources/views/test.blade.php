e317887004e75eabd1e7a0544de1a6cd1b7989b1
fgmasw <fgiraldoa.student.viu@gmail.com>
2024-10-12 19:07:27 -0300

    Update with changes 2024-10-12 19:07

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Prueba de Vite</h1>
+    <h1>Página de Prueba de Vite</h1>
     <p>Este es un contenido de prueba para verificar que Vite está funcionando.</p>
 @endsection
